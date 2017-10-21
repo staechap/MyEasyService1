029fad4f143385e119f23fb89424242bdf43c312
oramon <supachai@bic-com.com>
2017-10-21 15:44:47 +0700

    My Easy Service1 - Create main  fragment success / done

--- app/src/main/java/com/supachai/oramon/myeasyservice/MainActivity.java
+++ app/src/main/java/com/supachai/oramon/myeasyservice/MainActivity.java
@@ -3,11 +3,17 @@ package com.supachai.oramon.myeasyservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.supachai.oramon.myeasyservice.fragment.MainFragment;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+        }
+    } // main method
+} // main class
