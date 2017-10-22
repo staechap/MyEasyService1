709aab798eba9cdd7c85ccad7ef5ac4c1d208217
oramon <supachai@bic-com.com>
2017-10-22 10:59:48 +0700

    My Easy Service1 - Create main  fragment success / done
    - Start Fragment Page2
    
    -Day2 : Design UI page Fragment 2
    
    Finished on  go to Second + Back

--- app/src/main/java/com/supachai/oramon/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/com/supachai/oramon/myeasyservice/fragment/SecondFragment.java
@@ -7,6 +7,7 @@ import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.supachai.oramon.myeasyservice.R;
 
@@ -17,9 +18,31 @@ import java.util.zip.Inflater;
  */
 
 public class SecondFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Back Controller
+        backController();
+
+    } // Main Method
+
+    private void backController() {
+        Button button = getView().findViewById(R.id.btnBack);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_seconnd, container, false);
         return view;
     }
