7db0b24c2959c32dde8c3d65df6579df328cd279
oramon <supachai@bic-com.com>
2017-10-22 14:45:17 +0700

    My Easy Service1                      - Create main  fragment success / done
                         - Start Fragment Page2
    
                         -Day2 : Design UI page Fragment 2
    
                         Finished on  go to Second + Back Fragment
    
                         Design Drawer Menu OK  and Drawer Close
    
                         Add Hamberger Bar 1 / Done

--- app/src/main/java/com/supachai/oramon/myeasyservice/MainActivity.java
+++ app/src/main/java/com/supachai/oramon/myeasyservice/MainActivity.java
@@ -1,10 +1,13 @@
 package com.supachai.oramon.myeasyservice;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -37,6 +40,30 @@ public class MainActivity extends AppCompatActivity {
 
 
     } // main method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+         return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Add hamberger button
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    // Add hamberger button
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
 
     private void createToolBar() {
         toolbar = (Toolbar) findViewById(R.id.toolBarMain);
