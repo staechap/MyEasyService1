a51c20f0236592321534ed107eff15dab25ad1be
oramon <supachai@bic-com.com>
2017-10-22 13:42:52 +0700

    My Easy Service1 - Create main  fragment success / done
    - Start Fragment Page2
    
    -Day2 : Design UI page Fragment 2
    
    Finished on  go to Second + Back Fragment
    
    Design Drawer Menu OK

--- app/src/main/java/com/supachai/oramon/myeasyservice/MainActivity.java
+++ app/src/main/java/com/supachai/oramon/myeasyservice/MainActivity.java
@@ -1,19 +1,71 @@
 package com.supachai.oramon.myeasyservice;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.supachai.oramon.myeasyservice.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Add Fragment to Activity
+        addfragment(savedInstanceState);
+
+        //Setup Drawer
+        setupDrawer();
+
+        //Text Controller
+        textController();
+
+    } // main method
+
+    private void textController() {
+        TextView mainTextView = (TextView) findViewById(R.id.txtMainFragment);
+        TextView secondTextView = (TextView) findViewById(R.id.txtSecondFragment);
+        TextView exitTextView = (TextView) findViewById(R.id.txtExit);
+
+        // For MainFragment
+        mainTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+        // For SecondFragment
+        secondTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        // For Exit
+        exitTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.myDrawerLayout);
+    }
+
+    private void addfragment(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentFragmentMain, new MainFragment()).commit();
         }
-    } // main method
+    }
 } // main class
